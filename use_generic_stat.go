7defafd3b101511a31846ec4ab181f5f4e7c9826
djherbis <djherbis@gmail.com>
2016-01-24 13:15:46 -0800

    fixed undefined platformSpecificStat for non-windows builds

--- use_generic_stat.go
+++ use_generic_stat.go
@@ -3,3 +3,8 @@
 package times
 
 const hasPlatformSpecificStat = false
+
+// do not use, only here to prevent "undefined" method error.
+func platformSpecficStat(name string) (Timespec, error) {
+	return nil, nil
+}
