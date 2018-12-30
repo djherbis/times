9e76e532fac8fc4b17b82d0eb93b0d52fb96e1ff
Dustin H <djherbis@gmail.com>
2018-12-30 08:56:32 -0500

    add a directory to the test exe

--- example/main.go
+++ example/main.go
@@ -2,22 +2,48 @@ package main
 
 import (
 	"fmt"
-	"github.com/djherbis/times"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 	"time"
+
+	"github.com/djherbis/times"
 )
 
 func main() {
 	switch len(os.Args) {
 	case 1:
 		tempFile()
+		fmt.Println()
+		tempDir()
 
 	default:
 		printTimes(os.Args[1])
 	}
+}
+
+func tempDir() {
+	name, err := ioutil.TempDir("", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(name)
+	fmt.Println("# DIR: " + name)
+
+	symname := filepath.Join(filepath.Dir(name), "sym-"+filepath.Base(name))
+	if err := os.Symlink(name, symname); err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(symname)
+
+	newAtime := time.Now().Add(-10 * time.Second)
+	newMtime := time.Now().Add(10 * time.Second)
+	if err := os.Chtimes(name, newAtime, newMtime); err != nil {
+		log.Fatal(err)
+	}
+
+	printTimes(symname)
 }
 
 func tempFile() {
@@ -27,6 +53,7 @@ func tempFile() {
 	}
 	defer os.Remove(f.Name())
 	defer f.Close()
+	fmt.Println("# FILE: " + f.Name())
 
 	symname := filepath.Join(filepath.Dir(f.Name()), "sym-"+filepath.Base(f.Name()))
 	if err := os.Symlink(f.Name(), symname); err != nil {
@@ -44,10 +71,10 @@ func tempFile() {
 }
 
 func printTimes(name string) {
-	fmt.Println("Stat:", name)
+	fmt.Println("## Stat:", name)
 	printTimespec(times.Stat(name))
 
-	fmt.Println("\nLstat:", name)
+	fmt.Println("\n## Lstat:", name)
 	printTimespec(times.Lstat(name))
 }
 
