2973d42e13f7cb1c88e94cd59ef7ecfaaa8d9ae8
Dustin H <djherbis@gmail.com>
2018-12-31 12:32:38 -0500

    remove quotes so files are considered individually

--- .travis.sh
+++ .travis.sh
@@ -6,7 +6,7 @@ script() {
     then
         COVERALLS_PARALLEL=true
         go get github.com/axw/gocov/gocov github.com/mattn/goveralls golang.org/x/tools/cmd/cover
-        # go test -covermode=count -coverprofile=profile.cov
+        go test -covermode=count -coverprofile=profile.cov
 
         # add js coverage
         if [ "$TRAVIS_OS_NAME" == "linux" ];
@@ -14,9 +14,9 @@ script() {
             bash js.cover.sh
         fi
 
-        # PROFILES=`ls -dm profile.cov*`
-        # PROFILES=${PROFILES// /}
-        $HOME/gopath/bin/goveralls -coverprofile=profile.cov.js -service=travis-ci -repotoken $COVERALLS_TOKEN
+        PROFILES=`ls -dm profile.cov*`
+        PROFILES=${PROFILES// /}
+        $HOME/gopath/bin/goveralls -coverprofile=$PROFILES -service=travis-ci -repotoken $COVERALLS_TOKEN
     fi
 
     go get golang.org/x/lint/golint && golint ./...
