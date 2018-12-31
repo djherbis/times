af7c33d83c300c9e820e6e56ac7fb6682f4cce8d
Dustin H <djherbis@gmail.com>
2018-12-30 21:09:28 -0500

    remove extra get

--- .travis.sh
+++ .travis.sh
@@ -5,7 +5,7 @@ script() {
     if [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "$TRAVIS_OS_NAME" != "windows" ];
     then
         COVERALLS_PARALLEL=true
-        go get github.com/axw/gocov/gocov github.com/mattn/goveralls get golang.org/x/tools/cmd/cover
+        go get github.com/axw/gocov/gocov github.com/mattn/goveralls golang.org/x/tools/cmd/cover
         $HOME/gopath/bin/goveralls -service=travis-ci -repotoken $COVERALLS_TOKEN
     fi
 
