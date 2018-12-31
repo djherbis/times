12778e57fbc7fdba7da6e7ea06f8ac302406b340
Dustin H <djherbis@gmail.com>
2018-12-30 21:07:35 -0500

    reduce checks on windows, add error prop

--- .travis.sh
+++ .travis.sh
@@ -1,14 +1,22 @@
+#!/bin/bash
+set -e
+
 script() {
     if [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "$TRAVIS_OS_NAME" != "windows" ];
     then
         COVERALLS_PARALLEL=true
-        go get github.com/axw/gocov/gocov github.com/mattn/goveralls
-        if ! go get code.google.com/p/go.tools/cmd/cover; then go get golang.org/x/tools/cmd/cover;  fi
+        go get github.com/axw/gocov/gocov github.com/mattn/goveralls get golang.org/x/tools/cmd/cover
         $HOME/gopath/bin/goveralls -service=travis-ci -repotoken $COVERALLS_TOKEN
+
+        
     fi
 
-    go get golang.org/x/lint/golint && golint ./...
-    go vet
+    if [ "$TRAVIS_OS_NAME" != "windows" ];
+    then
+        go get golang.org/x/lint/golint && golint ./...
+        go vet
+    fi
+
     go test -bench=.* -v ./...
 }
 
