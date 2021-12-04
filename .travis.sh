462c67572eb036e131b89c83cfb0bcd53b325ef3
Dustin H <djherbis@gmail.com>
2021-12-04 16:10:13 -0500

    switch to staticcheck and other travis fixes

--- .travis.sh
+++ .travis.sh
@@ -13,13 +13,14 @@ script() {
             go test -covermode=count -coverprofile=profile.cov
         fi
 
-        go get github.com/axw/gocov/gocov github.com/mattn/goveralls golang.org/x/tools/cmd/cover
-        $HOME/gopath/bin/goveralls --coverprofile=profile.cov -service=travis-ci
+        go get github.com/axw/gocov/gocov golang.org/x/tools/cmd/cover
+        go install github.com/mattn/goveralls@latest
+        $GOPATH/bin/goveralls --coverprofile=profile.cov -service=travis-ci
     fi
 
     if [ -z "$JS" ];
     then
-        go get golang.org/x/lint/golint && golint ./...
+        go install honnef.co/go/tools/cmd/staticcheck@latest && $GOPATH/bin/staticcheck ./...
         go vet
         go test -bench=.* -v ./...
     fi
